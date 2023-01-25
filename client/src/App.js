dbf26bb7a4bdd225b891bc5dba18604771f2e2df
Nathan Love <nlovephd@gmail.com>
2023-01-25 13:56:58 -0600

    add a backend server

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import ProductDetails from './Components/ProductDetails/ProductDetails.js';
+import RatingsAndReviews from './Components/RatingsAndReviews/RatingsAndReviews.js';
+import QuestionsAndAnswers from './Components/QuestionsAndAnswers/QuestionsAndAnswers.js';
+import NavBar from './Components/NavBar/NavBar.js';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/products'/*, { body: 'What is a chicken?', name: 'Jimothy' }*/)
+    .then(res => {
+      console.log(res);
+      setProducts(res.data);
+      setProduct(products[0]); //fix this
+    })
+    .catch(err => console.log(err));
+  }, [])
+
+  const changeProduct = () => {
+    const random = Math.floor(Math.random() * products.length);
+    setProduct(products[random]);
+  }
+
+  return (
+    <div>
+      Welcome to our app!
+      <NavBar changeProduct={changeProduct}/>
+      <ProductDetails product={product} />
+      <QuestionsAndAnswers product={product} />
+      <RatingsAndReviews product={product} />
+      <p>Current Product is {JSON.stringify(product)}</p>
+    </div>
+  );
+}
+
+// this is Neb testing if I can push and pull from the remote repo
+export default App;
--- client/src/Components/RatingsAndReviews/RatingsAndReviews.js
+++ client/src/Components/RatingsAndReviews/RatingsAndReviews.js
@@ -2,7 +2,7 @@ import './RatingsAndReviews.css';
 
 function RatingsAndReviews() {
   return (
-
+    <div>I am ratings and reviews.</div>
   );
 }
 
