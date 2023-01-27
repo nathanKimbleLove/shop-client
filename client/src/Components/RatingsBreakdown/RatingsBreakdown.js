cbe2ec7e8c90acc7b03c7274b045561501e17de8
Nathan Love <nlovephd@gmail.com>
2023-01-26 19:07:58 -0600

    continuing with CSS

--- client/src/Components/RatingsBreakdown/RatingsBreakdown.js
+++ client/src/Components/RatingsBreakdown/RatingsBreakdown.js
@@ -6,22 +6,50 @@ function RatingsBreakdown() {
 
   return (
     <div className="ratingsBreakdown">
-      <div className="stars">
-        <span>1.5</span>
-        <span><BsStarFill /><BsStarHalf /><BsStar /><BsStar /><BsStar /></span>
+      <div className="averageStars">
+        <span className="primaryText averageStarsNumber">1.5</span>
+        <span className="averageStarsStars"><BsStarFill /><BsStarHalf /><BsStar /><BsStar /><BsStar /></span>
       </div>
       <div className="starCounts">
-        <div><BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /> 100</div>
-        <div><BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /><BsStar /> 50</div>
-        <div><BsStarFill /><BsStarFill /><BsStarFill /><BsStar /><BsStar /> 9</div>
-        <div><BsStarFill /><BsStarFill /><BsStar /><BsStar /><BsStar /> 8</div>
-        <div><BsStarFill /><BsStar /><BsStar /><BsStar /><BsStar /> 1200</div>
+        <div className="stars"><BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill />
+          <div className="emptyBar">
+            <div className="fullBar" style={{'width': '120px'}}></div>
+          </div>
+        </div>
+        <div className="stars"><BsStarFill /><BsStarFill /><BsStarFill /><BsStarFill /><BsStar />
+          <div className="emptyBar">
+            <div className="fullBar" style={{'width': '20'}}></div>
+          </div>
+        </div>
+        <div className="stars"><BsStarFill /><BsStarFill /><BsStarFill /><BsStar /><BsStar />
+          <div className="emptyBar">
+            <div className="fullBar" style={{'width': '20'}}></div>
+          </div>
+        </div>
+        <div className="stars"><BsStarFill /><BsStarFill /><BsStar /><BsStar /><BsStar />
+          <div className="emptyBar">
+            <div className="fullBar" style={{'width': '22px'}}></div>
+          </div>
+        </div>
+        <div className="stars"><BsStarFill /><BsStar /><BsStar /><BsStar /><BsStar />
+          <div className="emptyBar">
+            <div className="fullBar" style={{'width': '180px'}}></div>
+          </div>
+        </div>
       </div>
-      <div className="sizeBar" >size { /*WE WILL USE TWO SVGs AND USE A VAR TO TRANSFORM THE POINTER ACCURATELY */ }
-        <div>======================</div>
+      <div>
+        <span className="sizeBarRatings spacing">Size ratings:</span>
+        <div className="sizeBar"></div>
+        <div className="sizeBarRatings">
+          <span>Too small</span><span>Perfect</span><span>Too large</span>
+        </div>
       </div>
-      <div className="comfortBar" >comfort
-        <div>======================</div>
+      <div>
+        <span className="sizeBarRatings spacing">Comfort ratings:</span>
+        <div className="sizeBar"></div>
+        <div className="sizeBarRatings">
+          <span>Poor</span><span>Perfect</span>
+        </div>
       </div>
     </div>
   );
